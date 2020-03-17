97b1200e01aba381fd00bfaa5c1c4d60e1e3efeb
whatisthenote <jgj_junior@yahoo.com.br>
2020-03-17 16:34:28 -0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  selectCount,
+  incrementByAmount,
+  incrementAsync
+} from "./counter/counterSlice";
 
-function App() {
+export default function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      {count}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <input
+        value={incrementAmount}
+        onChange={e => setIncrementAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>
+        add ammount
+      </button>
+      <button onClick={() => dispatch(incrementAsync(Number(incrementAmount)))}>
+        async
+      </button>
     </div>
   );
 }
-
-export default App;
